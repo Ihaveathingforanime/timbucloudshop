a497eb7766b6f44236c6a56261a84fd9a6eafc05
Ihaveathingforanime <110555206+Ihaveathingforanime@users.noreply.github.com>
2024-07-09 23:00:08 +0100

    Create Logo.js

---
+++ Logo.js
@@ -0,0 +1,18 @@
+import './Logo.css'
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <div className="image-1">
+      </div>
+      <div className="group-86">
+        <span className="itune">
+          itune
+        </span>
+        <span className="intl-tech">
+          Intl. Tech.
+        </span>
+      </div>
+    </div>
+  )
+}
