5e919094686779f660d61c2eb28be44b861af537
Ihaveathingforanime <110555206+Ihaveathingforanime@users.noreply.github.com>
2024-07-09 23:12:48 +0100

    Create Description.js

---
+++ Description.js
@@ -0,0 +1,127 @@
+import './Description.css'
+
+export default function Description() {
+  return (
+    <div className="description">
+      <div className="container">
+        <div className="frame-36476">
+          <div className="group-84">
+            <span className="container">
+              1:20
+            </span>
+            <img className="vector" src="assets/vectors/Unknown" />
+          </div>
+          <div className="group-85">
+            <div className="elsignal">
+              <img className="vector-1" src="assets/vectors/Unknown" />
+            </div>
+            <div className="fontistowifi">
+              <img className="container-1" src="assets/vectors/Unknown" />
+              <img className="vector-4" src="assets/vectors/Unknown" />
+            </div>
+            <div className="fabattery">
+              <img className="vector-5" src="assets/vectors/Unknown" />
+            </div>
+          </div>
+        </div>
+        <div className="frame-36642">
+          <div className="frame-36480">
+            <img className="vector-9" src="assets/vectors/Unknown" />
+          </div>
+          <div className="favourite">
+            <div className="favorite-light">
+              <img className="vector-15" src="assets/vectors/Unknown" />
+            </div>
+          </div>
+        </div>
+        <div className="frame-36654">
+          <div className="frame-36651">
+            <div className="frame-36650">
+              <div className="frame-36645">
+                <div className="oraimo-smart-watch">
+                  Oraimo Smart Watch
+                </div>
+                <div className="frame-36644">
+                  <div className="rate">
+                    <img className="star-1" src="assets/vectors/Unknown" />
+                  </div>
+                  <div className="container-1">
+                    4.5
+                  </div>
+                </div>
+              </div>
+              <p className="one-of-the-valueable-featueres-of-oraimo-smart-wacth-is-that-it-equipped-with-sensors-to-monitor-heart-rate-steps-sleep-patterns-and-even-oxygen-levels">
+                <span className="one-of-the-valueable-featueres-of-oraimo-smart-wacth-is-that-it-equipped-with-sensors-to-monitor-heart-rate-steps-sleep-patterns-and-even-oxygen-levels-sub-7"></span><span></span>
+              </p>
+            </div>
+            <div className="frame-36649">
+              <div className="frame-36646">
+                <div className="favorite-light-1">
+                  <img className="vector-151" src="assets/vectors/Unknown" />
+                </div>
+              </div>
+              <div className="frame-36648">
+              </div>
+              <div className="frame-36647">
+              </div>
+            </div>
+          </div>
+          <div className="frame-36653">
+            <div className="frame-366531">
+              <div className="vector-404">
+              </div>
+              <span className="container-2">
+                1
+              </span>
+              <img className="group-87" src="assets/vectors/Unknown" />
+            </div>
+            <div className="frame-36486">
+              <div className="ngn-6000000">
+                NGN60,000.00
+              </div>
+              <div className="frame-36479">
+                <span className="buy-now">
+                  Buy Now
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-36614">
+          <div className="frame-36635">
+            <img className="home" src="assets/vectors/Unknown" />
+            <p className="home-1">
+              <span className="home-1-sub-0"></span><span></span>
+            </p>
+          </div>
+          <div className="frame-36636">
+            <div className="raphaelcart">
+              <img className="vector-6" src="assets/vectors/Unknown" />
+            </div>
+            <span className="cart">
+              Cart
+            </span>
+          </div>
+          <div className="frame-36638">
+            <div className="favorite">
+              <img className="vector-152" src="assets/vectors/Unknown" />
+            </div>
+            <span className="favourite-1">
+              Favourite
+            </span>
+          </div>
+          <div className="frame-36610">
+            <div className="account-circle">
+              <img className="icon" src="assets/vectors/Unknown" />
+            </div>
+            <span className="profile">
+              profile
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="frame-36485">
+      </div>
+    </div>
+  )
+}
