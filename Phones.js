6f16102c0474babc9d5c4be495bb31653f14a029
Ihaveathingforanime <110555206+Ihaveathingforanime@users.noreply.github.com>
2024-07-09 23:09:24 +0100

    Create Phones.js

---
+++ Phones.js
@@ -0,0 +1,134 @@
+import './Phones.css'
+
+export default function Phones() {
+  return (
+    <div className="phones">
+      <div className="frame-36490">
+        <div className="frame-36476">
+          <div className="group-84">
+            <span className="container">
+              1:20
+            </span>
+            <img className="vector" src="assets/vectors/Unknown" />
+          </div>
+          <div className="group-85">
+            <div className="elsignal">
+              <img className="vector-1" src="assets/vectors/Unknown" />
+            </div>
+            <div className="fontistowifi">
+              <img className="container-1" src="assets/vectors/Unknown" />
+              <img className="vector-4" src="assets/vectors/Unknown" />
+            </div>
+            <div className="fabattery">
+              <img className="vector-5" src="assets/vectors/Unknown" />
+            </div>
+          </div>
+        </div>
+        <div className="input-field">
+          <img className="search" src="assets/vectors/Unknown" />
+          <span className="type-amessage">
+            Search
+          </span>
+        </div>
+        <div className="products">
+          <div className="container">
+            <div className="frame-36478">
+              <span className="all">
+                All
+              </span>
+            </div>
+            <div className="frame-364791">
+              <span className="laptops">
+                Laptops
+              </span>
+            </div>
+            <div className="frame-36480">
+              <span className="desktop">
+                Desktop
+              </span>
+            </div>
+            <div className="frame-36482">
+              <span className="phones-1">
+                Phones
+              </span>
+            </div>
+          </div>
+          <div className="frame-36481">
+            <span className="cctv-camera">
+              Cctv Camera
+            </span>
+          </div>
+          <div className="frame-36483">
+            <span className="projectors">
+              Projectors
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="frame-36494">
+        <div className="frame-36489">
+          <div className="frame-36485">
+            <div className="favorite-light">
+              <img className="vector-15" src="assets/vectors/Unknown" />
+            </div>
+          </div>
+          <div className="frame-36488">
+            <div className="frame-36487">
+              <span className="apple-iphone-14-pro-max">
+                Apple IPhone 14 Pro Max
+              </span>
+              <span className="iinch-256-gb-nano-sim-gold">
+                6.7&#34; Iinch<br />
+                256GB <br />
+                Nano Sim <br />
+                Gold
+              </span>
+            </div>
+            <div className="frame-36486">
+              <div className="ngn-150000000">
+                NGN1,500,000.00
+              </div>
+              <div className="frame-36479">
+                <span className="buy">
+                  Buy
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="frame-36614">
+        <div className="frame-36635">
+          <img className="home" src="assets/vectors/Unknown" />
+          <p className="home-1">
+            <span className="home-1-sub-0"></span><span></span>
+          </p>
+        </div>
+        <div className="frame-36636">
+          <div className="raphaelcart">
+            <img className="vector-6" src="assets/vectors/Unknown" />
+          </div>
+          <span className="cart">
+            Cart
+          </span>
+        </div>
+        <div className="frame-36638">
+          <div className="favorite">
+            <img className="vector-151" src="assets/vectors/Unknown" />
+          </div>
+          <span className="favourite">
+            Favourite
+          </span>
+        </div>
+        <div className="frame-36610">
+          <div className="account-circle">
+            <img className="icon" src="assets/vectors/Unknown" />
+          </div>
+          <span className="profile">
+            profile
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
