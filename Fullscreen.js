fa6c331ad1fb85b6d16dfd7800406d77270326d2
Ihaveathingforanime <110555206+Ihaveathingforanime@users.noreply.github.com>
2024-07-09 23:01:52 +0100

    Create Fullscreen.js

---
+++ Fullscreen.js
@@ -0,0 +1,345 @@
+import './FullScreen.css'
+
+export default function FullScreen() {
+  return (
+    <div className="full-screen">
+      <div className="container-2">
+        <div className="frame-36490">
+          <div className="frame-36476">
+            <div className="group-84">
+              <span className="container">
+                1:20
+              </span>
+              <img className="vector" src="assets/vectors/Unknown" />
+            </div>
+            <div className="group-85">
+              <div className="elsignal">
+                <img className="vector-1" src="assets/vectors/Unknown" />
+              </div>
+              <div className="fontistowifi">
+                <img className="container" src="assets/vectors/Unknown" />
+                <img className="vector-4" src="assets/vectors/Unknown" />
+              </div>
+              <div className="fabattery">
+                <img className="vector-5" src="assets/vectors/Unknown" />
+              </div>
+            </div>
+          </div>
+          <div className="input-field">
+            <img className="search" src="assets/vectors/Unknown" />
+            <span className="type-amessage">
+              Search
+            </span>
+          </div>
+          <div className="products">
+            <div className="container-1">
+              <div className="frame-36478">
+                <span className="all">
+                  All
+                </span>
+              </div>
+              <div className="frame-364793">
+                <span className="laptops">
+                  Laptops
+                </span>
+              </div>
+              <div className="frame-364805">
+                <span className="desktop">
+                  Desktop
+                </span>
+              </div>
+              <div className="frame-36482">
+                <span className="phones">
+                  Phones
+                </span>
+              </div>
+            </div>
+            <div className="frame-36481">
+              <span className="cctv-camera">
+                Cctv Camera
+              </span>
+            </div>
+            <div className="frame-36483">
+              <span className="projectors">
+                Projectors
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="frame-36640">
+          <div className="frame-36489">
+            <div className="frame-36485">
+              <div className="favorite-light">
+                <img className="vector-15" src="assets/vectors/Unknown" />
+              </div>
+            </div>
+            <div className="frame-36488">
+              <div className="frame-36487">
+                <span className="oraimo-smart-watch">
+                  Oraimo Smart Watch
+                </span>
+                <span className="waterproof-blood-oxygen-24-fitness-pro">
+                  Waterproof Blood, Oxygen 24 Fitness Pro
+                </span>
+              </div>
+              <div className="frame-36486">
+                <div className="ngn-6000000">
+                  NGN60,000.00
+                </div>
+                <div className="frame-36479">
+                  <span className="buy">
+                    Buy
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-364891">
+            <div className="frame-364851">
+              <div className="favorite-light-1">
+                <img className="vector-151" src="assets/vectors/Unknown" />
+              </div>
+            </div>
+            <div className="frame-364881">
+              <div className="frame-364871">
+                <span className="dell-3180">
+                  Dell 3180
+                </span>
+                <span className="slim-webcam-touch-screen-hdmi-4-gb-ram-128-ssd">
+                  Slim<br />
+                  Webcam<br />
+                  Touch screen<br />
+                  Hdmi<br />
+                  4Gb Ram 128SSD
+                </span>
+              </div>
+              <div className="frame-364861">
+                <div className="ngn-15000000">
+                  NGN150,000.00
+                </div>
+                <div className="frame-36480">
+                  <span className="buy-1">
+                    Buy
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-36491">
+            <div className="frame-364852">
+              <div className="favorite-light-2">
+                <img className="vector-152" src="assets/vectors/Unknown" />
+              </div>
+            </div>
+            <div className="frame-364882">
+              <div className="frame-364872">
+                <span className="sandisk-ultra">
+                  Sandisk Ultra
+                </span>
+                <span className="usb-31-flash-drive-16-gb">
+                  Usb 3.1 Flash Drive <br />
+                  16gb
+                </span>
+              </div>
+              <div className="frame-364862">
+                <div className="ngn-2000000">
+                  NGN20,000.00
+                </div>
+                <div className="frame-364801">
+                  <span className="buy-2">
+                    Buy
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-36493">
+            <div className="frame-364853">
+              <div className="favorite-light-3">
+                <img className="vector-153" src="assets/vectors/Unknown" />
+              </div>
+            </div>
+            <div className="frame-364883">
+              <div className="frame-364873">
+                <span className="hp-elitebook-840-g-5">
+                  HP Elitebook 840 G5
+                </span>
+                <span className="core-i-5256-ssd-16-gb-ram-8-th-gen">
+                  core I5<br />
+                  256SSD<br />
+                  16Gb Ram<br />
+                  8th Gen
+                </span>
+              </div>
+              <div className="frame-364863">
+                <div className="ngn-27000000">
+                  NGN270,000.00
+                </div>
+                <div className="frame-364791">
+                  <span className="buy-3">
+                    Buy
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-364911">
+            <div className="frame-364854">
+              <div className="favorite-light-4">
+                <img className="vector-154" src="assets/vectors/Unknown" />
+              </div>
+            </div>
+            <div className="frame-364884">
+              <div className="frame-364874">
+                <span className="dell-latitude-7390">
+                  Dell Latitude 7390
+                </span>
+                <span className="core-i-516-gb-ram-2-in-1-touch-screen-8-th-gen">
+                  Core i5<br />
+                  16Gb Ram<br />
+                  2 in 1Touch screen<br />
+                  8th Gen
+                </span>
+              </div>
+              <div className="frame-364864">
+                <div className="ngn-29000000">
+                  NGN290,000.00
+                </div>
+                <div className="frame-364802">
+                  <span className="buy-4">
+                    Buy
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-364931">
+            <div className="frame-364855">
+              <div className="favorite-light-5">
+                <img className="vector-155" src="assets/vectors/Unknown" />
+              </div>
+            </div>
+            <div className="frame-364885">
+              <div className="frame-364875">
+                <span className="hp-elitebook-1040">
+                  HP Elitebook 1040
+                </span>
+                <span className="core-i-7256-ssd-16-gb-ram-8-th-gen">
+                  core I7<br />
+                  256SSD<br />
+                  16Gb Ram<br />
+                  8th Gen
+                </span>
+              </div>
+              <div className="frame-364865">
+                <div className="ngn-290000001">
+                  NGN290,000.00
+                </div>
+                <div className="frame-364803">
+                  <span className="buy-5">
+                    Buy
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-36492">
+            <div className="frame-364856">
+              <div className="favorite-light-6">
+                <img className="vector-156" src="assets/vectors/Unknown" />
+              </div>
+            </div>
+            <div className="frame-364886">
+              <div className="frame-364876">
+                <span className="lenovo-think-vision">
+                  Lenovo ThinkVision 
+                </span>
+                <span className="inch-546-mm-tft-lcd-1920-x-1080167-million-colours">
+                  21.5 inch (546mm)<br />
+                  TFT LCD<br />
+                  1920 x 1080<br />
+                  16.7 million colours
+                </span>
+              </div>
+              <div className="frame-364866">
+                <div className="ngn-12000000">
+                  NGN120,000.00
+                </div>
+                <div className="frame-364804">
+                  <span className="buy-6">
+                    Buy
+                  </span>
+                </div>
+              </div>
+              <p className="not-available">
+                <span className="not-available-sub-7"></span><span></span>
+              </p>
+            </div>
+          </div>
+          <div className="frame-364921">
+            <div className="frame-364857">
+              <div className="favorite-light-7">
+                <img className="vector-157" src="assets/vectors/Unknown" />
+              </div>
+            </div>
+            <div className="frame-364887">
+              <div className="frame-364877">
+                <span className="usb-type-xto-port">
+                  USB type x to port
+                </span>
+                <span className="in-1-vga-lan-port-4-xusb-30-sd-tf-card-hdmi-audio-port-usb-ctype-cfemale-to-usb-ctype-chub-adapter-dark-gray">
+                  11 in 1 VGA + LAN Port + 4 x USB 3.0 + SD / TF Card + HDMI + Audio Port + USB-C / Type-C Female to USB-C / Type-C HUB Adapter(Dark Gray)
+                </span>
+              </div>
+              <div className="frame-364867">
+                <div className="ngn-60000001">
+                  NGN60,000.00
+                </div>
+                <div className="frame-364792">
+                  <span className="buy-7">
+                    Buy
+                  </span>
+                </div>
+              </div>
+              <p className="not-available-1">
+                <span className="not-available-1-sub-7"></span><span></span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="frame-36613">
+        <div className="frame-36635">
+          <img className="home" src="assets/vectors/Unknown" />
+          <p className="home-1">
+            <span className="home-1-sub-0"></span><span></span>
+          </p>
+        </div>
+        <div className="frame-36636">
+          <div className="raphaelcart">
+            <img className="vector-6" src="assets/vectors/Unknown" />
+          </div>
+          <span className="cart">
+            Cart
+          </span>
+        </div>
+        <div className="frame-36638">
+          <div className="favorite">
+            <img className="vector-158" src="assets/vectors/Unknown" />
+          </div>
+          <span className="favourite">
+            Favourite
+          </span>
+        </div>
+        <div className="frame-36610">
+          <div className="account-circle">
+            <img className="icon" src="assets/vectors/Unknown" />
+          </div>
+          <span className="profile">
+            profile
+          </span>
+        </div>
+      </div>
+    </div>
+  )
+}
